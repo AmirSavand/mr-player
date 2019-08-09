d217da40bef19fccd899d6f9e17004d618b5174e
Amir Savand <amir@savandbros.com>
2019-08-09 23:26:14 +0300

    Add YouTube Data API v3 key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment: Environment = {
   production: true,
   name: 'production',
   api: 'https://playzem-api.herokuapp.com/',
+  youtube: 'AIzaSyBvPUNQMo3udOQ3EgSUF9_RMM1xA60-03I',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment: Environment = {
   production: false,
   name: 'development',
   api: 'https://playzem-api.herokuapp.com/',
+  youtube: 'AIzaSyBvPUNQMo3udOQ3EgSUF9_RMM1xA60-03I',
 };
